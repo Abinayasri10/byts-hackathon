3d2f8df008868b00d112ec9ad9aa3d49562156f5
Abinaya Sri J <abinayasrij.23cse@kongu.edu>
2026-02-02 10:55:11 +0530

    Initial commit

---
+++ placehub-master/frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
+import ProfileSetupPage from './pages/ProfileSetupPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
+import ShareExperienceLanding from './pages/ShareExperienceLanding.jsx'
+import ExperienceMetadataForm from './pages/ExperienceMetadataForm.jsx'
+import ExperienceRoundsForm from './pages/ExperienceRoundsForm.jsx'
+import ExperienceMaterialsForm from './pages/ExperienceMaterialsForm.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route 
+          path="/profile-setup" 
+          element={<ProtectedRoute><ProfileSetupPage /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/home" 
+          element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/share-experience" 
+          element={<ProtectedRoute><ShareExperienceLanding /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/share-experience/metadata" 
+          element={<ProtectedRoute><ExperienceMetadataForm /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/share-experience/rounds" 
+          element={<ProtectedRoute><ExperienceRoundsForm /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/share-experience/materials" 
+          element={<ProtectedRoute><ExperienceMaterialsForm /></ProtectedRoute>} 
+        />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
